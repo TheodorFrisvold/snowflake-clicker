70272525e5e2d78aafaca62ba0c944e7797796ac
TheodorFrisvold <theodorkmfrisvold@gmail.com>
2023-02-15 10:36:54 +0100

    rebranded to d2 fomo

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,17 @@
 var showFlakes;
+var ticksPerSecond = 20;
+var msPerTick = 1000/ticksPerSecond;
 
 function init(){
     //initializes game
     showFlakes = document.getElementById("showFlakes");
+    let x = document.cookie;
+    if (x != null) {
+        
+    }
 }
 
-setInterval(updateGame, 50);
+setInterval(updateGame, msPerTick);
 
 function clickedSnowflake(){
     Model.main.snowflakes += Model.main.snowflakesPerClick;
@@ -13,5 +19,20 @@ function clickedSnowflake(){
 function updateGame(){
     //updates game on a setInterval
     console.log("running updateGame");
-    showFlakes.innerHTML = `${Model.main.snowflakes} Snowflakes`;
+
+    //update Snowflakes
+    Model.main.snowflakes += Model.main.snowflakesPerSecond / ticksPerSecond;
+    showFlakes.innerHTML = `${parseInt(Model.main.snowflakes)} Silver`;
+
+}
+
+function buyUpgrade(type) {
+    if (type == shovel) {
+        Model.main.snowflakesPerClick += 1;
+        Model.main.snowflakesPerSecond += 1;
+    }
+}
+
+function saveGame(){
+    document.cookie = gameSave;
 }
